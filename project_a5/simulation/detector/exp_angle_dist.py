73fd0105493247365b8290855d7ed7e6f101b5bf
Nikola Mang <nikola.mang@tu-dortmund.de>
2021-05-24 14:47:19 +0200

    changed structure and finished ex. 9

--- project_a5/simulation/detector/exp_angle_dist.py
+++ project_a5/simulation/detector/exp_angle_dist.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import default_rng
 
 
 class ExponentialDeltaAngleDistribution(object):
@@ -55,20 +56,28 @@ class ExponentialDeltaAngleDistribution(object):
         # ---------
         #   Angle PDf | part a) (exercises/angle_pdf.py)
         #
-        # ---------------------------------------------------------------------
-        # --- Replace Code Here
-        # ---------------------------------------------------------------------
+        # define N
+        N = self.k/(2 * (1 - np.exp(- np.pi * self.k)))
 
-        # dummy solution to pass unit tests (this solution is not correct!)
         values = np.zeros_like(delta_dir)
         if np.isscalar(delta_dir):
             if delta_dir < -np.pi or delta_dir >= np.pi:
                 values = 0.
             elif delta_dir == 0.:
-                values = 1.
+                values = N
+            elif delta_dir >= -np.pi and delta_dir <= 0:
+                values = N * np.exp(delta_dir * self.k)
+            elif delta_dir > 0 and delta_dir < np.pi:
+                values = N * np.exp(- delta_dir * self.k)
         else:
-            mask = np.logical_or(delta_dir < -np.pi, delta_dir >= np.pi)
-            values[mask] = 0.
+            mask_neg = np.logical_and(delta_dir >= -np.pi, delta_dir <= 0)
+            values[mask_neg] = N * np.exp(delta_dir[mask_neg] * self.k)
+
+            mask_pos = np.logical_and(delta_dir > 0, delta_dir < np.pi)
+            values[mask_pos] = N * np.exp(- delta_dir[mask_pos] * self.k)
+
+            values[delta_dir == 0] = N
+            # rest can stay 0 as it is produced above
 
         return values
 
@@ -96,9 +105,10 @@ class ExponentialDeltaAngleDistribution(object):
         # ---------------------------------------------------------------------
         # --- Replace Code Here
         # ---------------------------------------------------------------------
-
-        # dummy solution to pass unit tests (this solution is not correct!)
         values = 0.5*np.ones_like(delta_dir)
+
+        # define N
+        N = self.k/(2 * (1 - np.exp(- np.pi * self.k)))
 
         # values below range have a cdf of zero, over range have cdf of 1
         if np.isscalar(delta_dir):
@@ -106,9 +116,25 @@ class ExponentialDeltaAngleDistribution(object):
                 values = 0.
             elif delta_dir >= np.pi:
                 values = 1.
+            elif delta_dir > -np.pi and delta_dir < 0:
+                values = (N/self.k) * (np.exp(delta_dir * self.k) - np.exp(- np.pi * self.k))
+            elif delta_dir > 0 and delta_dir < np.pi:
+                values = (N/self.k) * (1 - np.exp(-delta_dir * self.k)) + 1/2
+            elif delta_dir == 0:
+                values = 0.5
+
         else:
-            values[delta_dir <= -np.pi] = 0.
+            # masks for the cases
+            mask_case1 = np.logical_and(delta_dir > -np.pi, delta_dir < 0)
+            mask_case2 = np.logical_and(delta_dir > 0, delta_dir < np.pi)
+
+            values[mask_case1] = (N/self.k) * (np.exp(delta_dir[mask_case1] * self.k) - np.exp(- np.pi * self.k))
+            values[mask_case2] = (N/self.k) * (1 - np.exp(-delta_dir[mask_case2] * self.k)) + 1/2
+
+            # special cases
+            values[delta_dir == 0] = 0.5
             values[delta_dir >= np.pi] = 1.
+            values[delta_dir <= -np.pi] = 0.
 
 
         return values
@@ -137,12 +163,14 @@ class ExponentialDeltaAngleDistribution(object):
         # ---------
         #   Angle PDf | part c) (exercises/angle_pdf.py)
         #
-        # ---------------------------------------------------------------------
-        # --- Replace Code Here
-        # ---------------------------------------------------------------------
 
-        # dummy solution to pass unit tests (this solution is not correct!)
+        # define N
+        N = self.k/(2 * (1 - np.exp(- np.pi * self.k)))
+
+        # make value array
         values = np.zeros_like(q)
+
+        # in case q is just one value
         if np.isscalar(q):
             if q == 0.:
                 values = -np.pi
@@ -150,11 +178,22 @@ class ExponentialDeltaAngleDistribution(object):
                 values = 0.
             elif q == 1.:
                 values = np.pi
+            elif q > 0 and q < 0.5:
+                values = np.log(self.k * q / N + np.exp(- np.pi * self.k)) /self.k
+            elif q > 0.5 and q < 1:
+                values = - np.log(1 - (self.k/N) * (q - 1/2))/self.k
+             
         else:
+            # special cases first
             values[q == 0.0] = -np.pi
             values[q == 0.5] = 0.
             values[q == 1.0] = np.pi
 
+            mask_lower = np.logical_and(q > 0, q < 0.5) # formula for lower percentage
+            values[mask_lower] = np.log(self.k * q[mask_lower] / N + np.exp(- np.pi * self.k)) /self.k
+
+            mask_higher = np.logical_and(q > 0.5, q < 1.) # formula for higher percentage
+            values[mask_higher] = - np.log(1 - (self.k/N) * (q[mask_higher] - 1/2))/self.k
 
         return values
 
@@ -182,8 +221,8 @@ class ExponentialDeltaAngleDistribution(object):
         # ---------------------------------------------------------------------
         # --- Replace Code Here
         # ---------------------------------------------------------------------
-        # dummy solution to pass unit tests (this solution is not correct!)
-        samples = np.ones(size)
 
+        u = random_state.uniform(0, 1, size) # random uniform numbers
+        samples = self.ppf(u) # transform to desired distribution
 
         return samples
